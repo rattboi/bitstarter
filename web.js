db90823087472fa980c9194d1a5039c977c51d56
rattboi <rattboi@gmail.com>
2013-07-08 01:31:32 +0000

    Read index.html from web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
+ 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buffer = fs.readFileSync('index.html');
+  out = buffer.toString();
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
