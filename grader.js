63bccde0327b993cd938b7e264ad7fcd88d4efc2
rattboi <rattboi@gmail.com>
2013-07-10 23:32:12 +0000

    Working grader.js. Not happy w/ code duplication

--- grader.js
+++ grader.js
@@ -2,7 +2,6 @@
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing
-
 */
 
 var fs = require('fs');
@@ -19,17 +18,6 @@ var assertFileExists = function(infile) {
         process.exit(1);
     }
     return instr;
-};
-
-var getFileWithRestler = function(inurl) {
-    rest.get(inurl).on('complete', function(data) {
-	if (data instanceof Error) {
-	    console.log("%s could not be fetched. Exiting.", inurl);
-	    process.exit(1);
-	}
-	else
-	    return data;
-   });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -55,15 +43,46 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function(checksfile) {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+	    $ = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for (var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    console.log(JSON.stringify(out, null, 4));
+        }
+    };
+    return response2console;
+};
+
+var getFileWithRestler = function(inurl, checksfile) {
+    var comp_func = buildfn(checksfile);
+    rest.get(inurl).on('complete', comp_func);
+};
+
 if (require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'URI to check', clone(getFileWithRestler))
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'URI to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url)
+    {
+	var checkJson = getFileWithRestler(program.url, program.checks);
+    }
+    else
+    {
+        program.file = assertFileExists(program.file);
+	var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
